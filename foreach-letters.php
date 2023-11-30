4fbeb472e77cd0191049865e5a5240555fe830df
théau Yapi <theau.yapi@ynov.com>
2023-11-30 15:26:25 +0100

    streamdeck upload

--- foreach-letters.php
+++ foreach-letters.php
@@ -1,3 +1,6 @@
 <?php
 
-echo "A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z";
+foreach (range('A', 'Y') as $letter) {
+    echo $letter . ' | ';
+}
+echo "Z";
