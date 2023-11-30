fbc31736a1e177710cd0be5dec6446951769c065
théau Yapi <theau.yapi@ynov.com>
2023-11-30 11:07:31 +0100

    premier code php de moi wesh

---
+++ primitive-declarations.php
@@ -0,0 +1,32 @@
+<?php // We tell our file / browser that we’re gonna write/execute PHP code
+
+
+echo "Hello !";
+echo "Hello !";
+echo "Hello !";
+
+$myVariable = "Easy no?";
+$maVariable = 5 ;
+$myVariable = "Now I am assigned as a string! Fabulous.";
+//$var 1 = "toto" ; // No space
+//$var-1 = "toto" ; // No dash
+//$-var = "toto" ; // No dash
+//$20var = "toto" ; // Cannot start with a number
+//var = "toto" ; // Necessarily a $ in front of the name
+
+$firstStr = "ouais bonjour c'est moi wesh";
+$firstEscapeStr = "c'est toujours moi mais avec un retour a la ligne \n";
+$firstBool = TRUE;
+$firstInteger = 25;
+$firstFloat = 14.18;
+$firstNull = NULL;
+$firstArray = [2, NULL, 'Hi', 5.5, FALSE];
+
+print_r($firstArray);
+print_r($firstBool);
+print_r($firstEscapeStr);
+print_r($firstFloat);
+print_r($firstInteger);
+print_r($firstNull);
+print_r($firstStr);
+print_r($myVariable);
