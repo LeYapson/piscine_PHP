2d888d7f3071c7cdaa83001ed3850d08bfe1b4a1
théau Yapi <theau.yapi@ynov.com>
2023-11-30 16:37:12 +0100

    streamdeck upload

--- reverser.php
+++ reverser.php
@@ -4,3 +4,11 @@ function reverse(string $str): string
 {
     return strrev($str);
 }
+
+function isPalindrome(string $str): bool
+{
+    $revstr = strrev($str);
+    if ($str == $revstr) {
+        return TRUE;
+    }
+}
