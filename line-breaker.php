8c355f676d90d49e54420055d4e12637384ef9d7
théau Yapi <theau.yapi@ynov.com>
2023-11-30 16:46:42 +0100

    streamdeck upload

--- line-breaker.php
+++ line-breaker.php
@@ -1,29 +1,37 @@
 <?php
 
-function breakLines($string, $maxLength): string
-{
-    // Explode the input string into an array of words
-    $words = explode(' ', $string);
+function breakLines($string, $maxLength) {
+    // Split the input string into lines
+    $lines = explode("\n", $string);
 
     // Initialize variables
-    $currentLine = '';
     $result = '';
 
-    // Iterate through each word
-    foreach ($words as $word) {
-        // Check if adding the word to the current line exceeds the maximum length
-        if (strlen($currentLine . $word) <= $maxLength) {
-            // If not, add the word to the current line
-            $currentLine .= $word . ' ';
-        } else {
-            // If it exceeds the maximum length, start a new line
-            $result .= rtrim($currentLine) . "\n";
-            $currentLine = $word . ' ';
+    // Iterate through each line
+    foreach ($lines as $line) {
+        // Explode the line into an array of words
+        $words = explode(' ', $line);
+        $currentLine = '';
+
+        // Iterate through each word
+        foreach ($words as $word) {
+            // Check if adding the word to the current line exceeds the maximum length
+            if (strlen($currentLine . $word) <= $maxLength) {
+                // If not, add the word to the current line
+                $currentLine .= $word . ' ';
+            } else {
+                // If it exceeds the maximum length, start a new line
+                $result .= rtrim($currentLine) . "\n";
+                $currentLine = $word . ' ';
+            }
         }
+
+        // Add the last line to the result
+        $result .= rtrim($currentLine) . "\n";
     }
 
-    // Add the last line to the result
-    $result .= rtrim($currentLine);
+    // Remove the trailing newline character
+    $result = rtrim($result);
 
     return $result;
 }
