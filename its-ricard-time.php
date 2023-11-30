a16389cb120714c368efaef9d4967597c0bc810c
théau Yapi <theau.yapi@ynov.com>
2023-11-30 15:05:42 +0100

    streamdeck upload

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -1,12 +1,12 @@
 <?php
 
-$nbrRicard = 0;
+$nbrRicard = 1;
 do {
   echo "Come on ricard number ", $nbrRicard, "\n";
   $nbrRicard++;
-  if ($nbrRicard == 3):
+  if ($nbrRicard == 4):
     echo "I'll have to stop soon! \n";
-    elseif ($nbrRicard ==5):
+    elseif ($nbrRicard ==6):
         echo "I am no longer very fresh ... \n";
     endif;
 } while($nbrRicard < 8);
