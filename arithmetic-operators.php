f9271cb9dacebad34d92273eba08bc1d76d6bc2e
théau Yapi <theau.yapi@ynov.com>
2023-11-30 11:14:44 +0100

    streamdeck upload

---
+++ arithmetic-operators.php
@@ -0,0 +1,16 @@
+<?php // We tell our file / browser that we’re gonna write/execute PHP code
+
+$firstNbr = 100;
+$secondNbr = 35;
+
+$firstSum = $firstNbr + $secondNbr;
+$firstSub = $firstNbr - $secondNbr;
+$firstMult = $firstNbr * $secondNbr;
+$firstDivision = $firstNbr / $secondNbr;
+$firstModulus = $firstNbr % $secondNbr;
+
+print_r($firstSum);
+print_r($firstSub);
+print_r($firstMult);
+print_r($firstDivision);
+print_r($firstModulus);
