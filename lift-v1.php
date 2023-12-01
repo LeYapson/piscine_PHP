b6947c1f77a6d2bdee7947f37f295b0b851a7239
LeYapson <theau.yapi@ynov.com>
2023-12-01 15:52:25 +0100

    les exos de php a la con la

--- lift-v1.php
+++ lift-v1.php
@@ -14,31 +14,19 @@ function getFloor(int $currentFloor, ?int $requestedFloor, array $calledFloors):
 
     // Find the nearest called floor above the current floor
     $nearestFloorAbove = null;
-    $minDistanceAbove = PHP_INT_MAX;
 
     foreach ($calledFloors as $floor) {
-        if ($floor > $currentFloor) {
-            $distance = abs($floor - $currentFloor);
-
-            if ($distance < $minDistanceAbove) {
-                $minDistanceAbove = $distance;
-                $nearestFloorAbove = $floor;
-            }
+        if ($floor > $currentFloor && ($nearestFloorAbove === null || abs($floor - $currentFloor) < abs($nearestFloorAbove - $currentFloor))) {
+            $nearestFloorAbove = $floor;
         }
     }
 
     // Find the nearest called floor below the current floor
     $nearestFloorBelow = null;
-    $minDistanceBelow = PHP_INT_MAX;
 
     foreach ($calledFloors as $floor) {
-        if ($floor < $currentFloor) {
-            $distance = abs($floor - $currentFloor);
-
-            if ($distance < $minDistanceBelow) {
-                $minDistanceBelow = $distance;
-                $nearestFloorBelow = $floor;
-            }
+        if ($floor < $currentFloor && ($nearestFloorBelow === null || abs($floor - $currentFloor) < abs($nearestFloorBelow - $currentFloor))) {
+            $nearestFloorBelow = $floor;
         }
     }
 
@@ -51,5 +39,35 @@ function getFloor(int $currentFloor, ?int $requestedFloor, array $calledFloors):
 
     // Otherwise, there are no called floors closer than the requested floor, so go to the requested floor
     return $requestedFloor;
+}
+
+function getDirection(int $currentFloor, ?int $requestedFloor, array $calledFloors): int
+{
+    // If no movement is needed
+    if ($requestedFloor === null && empty($calledFloors)) {
+        return 0;
+    }
+
+    // If a floor is requested
+    if ($requestedFloor !== null) {
+        if ($requestedFloor > $currentFloor) {
+            return 1;
+        } elseif ($requestedFloor < $currentFloor) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    // If there are called floors, go to the nearest one
+    $nearestFloor = getFloor($currentFloor, null, $calledFloors);
+
+    if ($nearestFloor === null) {
+        return 0;
+    } elseif ($nearestFloor > $currentFloor) {
+        return 1;
+    } else {
+        return -1;
+    }
 }
 
