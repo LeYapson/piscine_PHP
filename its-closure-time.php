aeca1d039ddcd4b82ab0403b3778db0894027032
LeYapson <theau.yapi@ynov.com>
2023-12-04 17:37:23 +0100

    les exos de php a la con la

---
+++ its-closure-time.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Create an f function that returns an associative array containing 3 elements. The elements (in that order) are '+', '-', and '*'. The values ​​for each element are references for objects that represent functions.
+
+    The first is able to take two numbers and return their sum.
+    The second is able to take two numbers and return their difference.
+    The third is able to take two numbers and return their multiplications.
+
+Be careful, the function f does not take any parameters !!
+
+Trick : A function can return another function. Using anonymous functions, you'll see it's cool :D
+
+Trick 2 (because we're cool): Here are 2 ways to declare an anonymous function
+*/
+
+$f = function () {
+    return [
+        '+' => function ($a, $b) {
+            return $a + $b;
+        },
+        '-' => function ($a, $b) {
+            return $a - $b;
+        },
+        '*' => function ($a, $b) {
+            return $a * $b;
+        },
+    ];
+};
