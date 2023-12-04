79427e75df2cfcd69011c5e740e558834400fa46
LeYapson <theau.yapi@ynov.com>
2023-12-04 15:12:07 +0100

    les exos de php a la con la

--- recursive.php
+++ recursive.php
@@ -1,17 +1,12 @@
 <?php
 
-function factorial(float $n): float {
-    // Base case: factorial of 0 or 1 is 1
-    if ($n == 0 || $n == 1) {
+function factorial(int $n): int
+{
+    if ($n === 0 || $n === 1) {
         return 1;
     } else {
-        // Recursive case: n! = n * (n-1)!
         return $n * factorial($n - 1);
     }
 }
 
-// Example usage:
-$number = 5;
-$result = factorial($number);
-echo "The factorial of $number is: $result";
-
+echo factorial(1);
