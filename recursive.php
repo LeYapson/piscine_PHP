2025908c344e6eec071093ce48327cae49b48f06
LeYapson <theau.yapi@ynov.com>
2023-12-04 15:09:17 +0100

    les exos de php a la con la

--- recursive.php
+++ recursive.php
@@ -9,3 +9,9 @@ function factorial(float $n): float {
         return $n * factorial($n - 1);
     }
 }
+
+// Example usage:
+$number = 5;
+$result = factorial($number);
+echo "The factorial of $number is: $result";
+
