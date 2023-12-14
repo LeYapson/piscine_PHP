972fa9a22db52c89be7e88f7e908d2f888205b4b
LeYapson <theau.yapi@ynov.com>
2023-12-14 17:43:47 +0100

    les exos de php a la con la

--- invert-binary-tree.php
+++ invert-binary-tree.php
@@ -1,21 +1,11 @@
 <?php
 
-function myArrayMap(?callable $callback, array $array1, array ...$arrays)
-{
-    $result = [];
-    foreach ($array1 as $key => $value) {
-        $temp = [$value];
-        foreach ($arrays as $arr) {
-            $temp[] = $arr[$key];
-        }
-        $result[] = $callback ? $callback($temp) : $temp;
+function invertTree(?BinaryNode $root): ?BinaryNode {
+    if ($root === null) {
+        return null;
     }
-    return $result;
+    $temp = $root->left;
+    $root->left = invertTree($root->right);
+    $root->right = invertTree($temp);
+    return $root;
 }
-// Test #5
-$result5 = myArrayMap(static fn ($n) => $n * $n * $n, [1, 2, 3, 4, 5]);
-var_dump($result5 === [[1], [8], [27], [64], [125]]);
-// Test #8
-$result8 = myArrayMap(null, [1, 2, 3], ['one', 'two', 'three'], ['uno', 'dos', 'tres']);
-var_dump($result8 === [[1, 'one', 'uno'], [2, 'two', 'dos'], [3, 'three', 'tres']]);
-?>
