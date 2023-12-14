787ab6dc8923c0888222cb465f2edf0710974220
LeYapson <theau.yapi@ynov.com>
2023-12-14 17:26:44 +0100

    les exos de php a la con la

--- dna-difference.php
+++ dna-difference.php
@@ -1,37 +1,16 @@
 <?php
-
-/*Geneticists need to calculate the differences between two strands of DNA represented, for example, as follows :
-
-GAGCCTACTAACGGGAT
-CATCGTAATGACGGCCT
-^ ^ ^  ^ ^    ^^
-
-    The differences are represented by ^. For example here the answer is 7.
-
-Create a dnaDiff function which takes two strings as mandatory parameters and returns the number of differences between the two strands of DNA. If the two DNA strands are not the same size return false.
-
-You are not allowed to use the native array_diff or array_diff_assoc functions, make your own. */
-
-function dnaDiff(string $str1, string $str2) : int
-{
-    $arr1 = str_split($str1);
-    $arr2 = str_split($str2);
-    $count = 0;
-    if (sizeof($arr1) != sizeof($arr2))
-    {
-        return 0;
+function dnaDiff($left, $right) : int|bool {
+    $diff = 0;
+    if (strlen($left) !== strlen($right)) {
+        return false;
     }
-    else 
-    {
-        for ($i = 0; $i < sizeof($arr1); $i++)
-        {
-            if ($arr1[$i] != $arr2[$i])
-            {
-                $count++;
-            }
+    for ($i = 0; $i < max(strlen($left), strlen($right)); $i++) {
+        if ($i >= strlen($left) || $i >= strlen($right)) break;
+        $firstChar = $left[$i];
+        $lastChar  = $right[$i];
+        if ($firstChar !== $lastChar) {
+            $diff++;
         }
     }
-    return $count;
+    return $diff;
 }
-print_r(dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT'));
-print_r(dnaDiff('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCTAZA'));
