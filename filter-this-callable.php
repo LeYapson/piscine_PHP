c90101eb176e6bedc27806302e71057e64d2182e
LeYapson <theau.yapi@ynov.com>
2023-12-14 17:29:49 +0100

    les exos de php a la con la

---
+++ filter-this-callable.php
@@ -0,0 +1,15 @@
+<?php
+
+function myArrayFilter(array $array, ?callable $callback = null): array
+{
+    if ($callback === null) {
+        $callback = static fn ($n) => $n;
+    }
+    $result = [];
+    foreach ($array as $key => $value) {
+        if ($callback($value)) {
+            $result[$key] = $value;
+        }
+    }
+    return $result;
+}
